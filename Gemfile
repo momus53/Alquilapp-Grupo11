f306618584e930e4c7920eae81fc0a43a80cd2da
Joaquin Matto <joakomatto@gmail.com>
2022-10-26 16:29:45 -0300

    Bootstrap instalado

--- Gemfile
+++ Gemfile
@@ -9,6 +9,9 @@ gem "rails", "~> 7.0.4"
 #EL HIRB
 gem "hirb"
 
+#bootstrap
+gem 'bootstrap', '~> 5.2.2'
+
 # The original asset pipeline for Rails [https://github.com/rails/sprockets-rails]
 gem "sprockets-rails"
 
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -7,6 +7,7 @@ Rails.application.config.assets.version = "1.0"
 # Rails.application.config.assets.paths << Emoji.images_path
 
 # Precompile additional assets.
+Rails.application.config.assets.precompile += %w(bootstrap.min.js popper.js)
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
