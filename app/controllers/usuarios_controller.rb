3116acabf5962d257287a013434fbf8629ceae40
Juan Guido Zottig <juanzottig@gmail.com>
2022-11-21 11:52:29 -0300

    Testeo con form de registro

--- app/controllers/usuarios_controller.rb
+++ app/controllers/usuarios_controller.rb
@@ -1,7 +1,12 @@
 class UsuariosController < ApplicationController
-
     def show
         @usuario = Usuario.last
     end
-
+    
+    def new
+        @usuario = Usuario.new(params[@usuario])
+    end
+    def create
+        @usuario.save
+    end
 end
--- app/models/usuario.rb
+++ app/models/usuario.rb
@@ -1,3 +1,4 @@
 class Usuario < ApplicationRecord
-
+    has_one_attached :active_storage_blobs
+    has_one_attached :image
 end
